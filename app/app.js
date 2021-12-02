4a588dd32aa826e4d39beafbf83d5552724eb9ce
HishamRaihan <mr.hraihan@gmail.com>
2021-12-02 15:50:56 -0500

    complete front-end

--- app/app.js
+++ app/app.js
@@ -3,7 +3,24 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
-
+const authEvents = require('./auth/events');
+const destinationEvents = require('./destinations/events');
 $(() => {
-  // your JS code goes here
-})
+	// your JS code goes here
+	authEvents.addHandlers();
+
+	$('#destinations-index').on('click', destinationEvents.onIndexDestinations);
+	$('#destinations-show').on('submit', destinationEvents.onShowDestination);
+	$('#destinations-create').on('submit', destinationEvents.onCreateDestination);
+	$('#delete-destination').on('submit', destinationEvents.onDestroyDestination);
+	$('#destinations-display').on(
+		'click',
+		'.destinations-destroy-dynamic',
+		destinationEvents.onDynamicDestroyDestination
+	);
+	$('#destinations-display').on(
+		'submit',
+		'.destinations-update-dynamic',
+		destinationEvents.onDynamicUpdateDestination
+	);
+});
